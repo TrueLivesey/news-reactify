b4ae92f276bf9d0cd96609f849b65893cfa6f2af
ElseNimic <nekpavlov@gmail.com>
2025-02-20 15:58:58 +0300

    add prop types for all react files and skeleton for news and banner

---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.scss';
+import PropTypes from 'prop-types';
+
+export const Skeleton = ({ count = 1, type = 'banner' }) => {
+  return (
+    <>
+      {count > 1 ? (
+        <ul className={styles.list}>
+          {[...Array(count)].map((_, index) => (
+            <li key={index} className={type === 'banner' ? styles.banner : styles.item}></li>
+          ))}
+        </ul>
+      ) : (
+        <li className={type === 'banner' ? styles.banner : styles.item}></li>
+      )}
+    </>
+  );
+};
+
+Skeleton.propTypes = {
+  count: PropTypes.number,
+  type: PropTypes.oneOf(['banner', 'item']),
+};
